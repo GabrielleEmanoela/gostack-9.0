46a0f65afe5083dff96b861675b5919d61908fa8
˜Gabrielle <biela.a09@gmail.com>
2021-04-15 16:24:59 -0300

    Criando meu primeira rota *GET* Api

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
-console.log(express);
+
+const server = express();
+
+server.get('/teste', (req, res) => {  
+    return res.send('Hello')
+});
+
+server.listen(3000);
