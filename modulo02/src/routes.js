d91ecf8f147e28779726c443cdb20b77abc5ca7d
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-20 20:57:37 -0300

    Criando loader de models.

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import user from './app/models/user';
 const routes = new Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gabrielle Silva',
+    email: 'biela09@gmail.com',
+    password_hash: '12345678',
+  });
+
   return res.json({ message: 'Hello world' });
 });
 
