43a624174001b886083e7c16ab6747c7b605e303
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-20 09:31:04 -0300

    Finalizando a importação do sucrase.

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,10 +1,8 @@
-//importando apenas o router
-import { Router }  from 'express' ;
+import { Router } from "express";
 const routes = new Router();
 
 
-// criando rota get.
 routes.get('/', (req, res) => {
     return res.json({ message: 'Hello world' })
 })
-export default  routes; //exportando minhas rotas.
+export default routes; //exportando minhas rotas.
