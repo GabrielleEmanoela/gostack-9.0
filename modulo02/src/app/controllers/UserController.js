5e4a02f1ee319fbbf192c9d3758a746905c9238f
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-20 23:18:38 -0300

    Vendo se o e-mail do usuario ja existe.

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -2,6 +2,10 @@ import User from '../models/User';
 
 class UseController {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User alredy exists' });
+    }
     const user = await User.create(req.body);
     return res.json();
   }
