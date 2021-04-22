13a33f455eac2317851084431fbcb6dd32d8eb4f
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-22 16:54:12 -0300

    Update do usuário.

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -7,7 +7,7 @@ class UseController {
       return res.status(400).json({ error: 'User alredy exists' });
     }
     //Retornando os campos necessarios:
-    const { id, name, email, provider } = await User.create(req.body, {});
+    const { id, name, email, provider } = await User.create(req.body);
     return res.json({
       id,
       name,
@@ -17,7 +17,29 @@ class UseController {
   }
 
   async update(req, res) {
-    return res.json;
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'User alredy exists' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
